60126261d6a59d94226537e2cb3be2d610181a3a
Bob King <bob@getready1.com>
2021-08-14 14:17:57 -0400

    add paint coverage calculator area_calc.py

---
+++ day8/area_calc.py
@@ -0,0 +1,31 @@
+from os import system, name
+from math import ceil
+
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+def calculate_coverage(height, width, coverage):
+    area = (height * width)
+    cans_required = area / coverage
+    return ceil(cans_required)
+
+
+clear()
+
+height = int(input("What is the height of the wall? "))
+width = int(input("What is the  of the wall? "))
+coverage = int(input("What is the per can? "))
+
+total_cans = calculate_coverage(height, width, coverage)
+
+print(f"HEIGHT: {height}")
+print(f"WIDTH: {width}")
+print(f"COVERAGE: {coverage}")
+print(f"You will need {total_cans} cans of paint.")
