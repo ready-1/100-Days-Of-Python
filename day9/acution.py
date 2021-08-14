32af92498ec6def74f3a96ee661de2c1ca428518
Bob King <bob@getready1.com>
2021-08-14 17:59:37 -0400

    auction.py working

---
+++ day9/acution.py
@@ -0,0 +1,56 @@
+from os import system, name
+from art import logo as logo
+
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+def display_logo():
+    print(logo + "\n")
+
+
+def ask_for_bid(bid_index):
+    name = input("What is your name? ")
+    bid = float(input("What is your bid? "))
+    more_bidders = True
+    if input("Are there anymore bidders? ").lower() == "n":
+        more_bidders = False
+    clear()
+    return {"bid_index": bid_index, "name": name, "bid": bid, "more_bidders": more_bidders}
+
+
+def take_bids():
+    bids = []
+    bid = {"name": None, "bid": None, "more_bidders": True}
+    bid_index = 0
+
+    while bid["more_bidders"] == True:
+        bid = ask_for_bid(bid_index)
+        bids.append(bid)
+        bid_index += 1
+
+    return bids
+
+
+def display_winner(bids):
+    winner = {"name": None, "bid": 0.00}
+    for bid in bids:
+        if bid["bid"] > winner["bid"]:
+            winner["name"] = bid["name"]
+            winner["bid"] = bid["bid"]
+
+    print(f'The winner is {winner["name"]} with a bid of ${winner["bid"]}')
+
+
+def main():
+    bids = {}
+    clear()
+    display_logo()
+    bids = take_bids()
+    display_winner(bids)
