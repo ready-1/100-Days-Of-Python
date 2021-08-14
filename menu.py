377907cf71dd86a3be2d90b5307a506d03ba846c
Bob King <bob@getready1.com>
2021-08-13 23:42:38 -0400

    add day6hangmanOOP and clean up exit()

--- menu.py
+++ menu.py
@@ -2,14 +2,17 @@ from bk_utils import clear
 
 
 menu_items = [
-    ["Day 6 - Hangman", "from day6hangman import hangman as hangman", "hangman.main_loop()"]
+    ["Day 6 - Hangman", "from day6hangman import hangman as hangman",
+        "hangman.main_loop()"],
+    ["Day 6 - Hangman OOP", "from day6hangmanOOP import hangmanOOP as hangmanOOP",
+        "hangmanOOP.play()"],
 ]
 
 
 def display_menu():
     clear()
     print("       MENU\n========================")
-    for i in range(0,len(menu_items)):
+    for i in range(0, len(menu_items)):
         print(f'{i + 1}: {menu_items[i][0]}')
     print("Q: Quit\n")
 
@@ -23,9 +26,11 @@ def menu():
         display_menu()
         choice = input("Option: ")
         if choice.lower() == "q":
+            clear()
             exit()
 
         print(dir())
         exec(f"{menu_items[int(choice) - 1][2]}")
+
 
 menu()
