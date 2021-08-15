6d81dc6ea21723c4e4e63ccfd0268a3ab7c8a9f6
Bob King <bob@getready1.com>
2021-08-15 15:02:27 -0400

    Add loop w/ new, cont & quit opts. some formatting

--- day10/calculator.py
+++ day10/calculator.py
@@ -91,25 +91,62 @@ operations = {
     "/": divide
 }
 
-# clear the screen
+
+def calculator():
+    """Calculator Loop
+
+    Main Loop for the calculator.  Allows recursion to continue calculations with 
+    num1 set to the answer from the last calculation.
+    """
+    # clear the screen
+    clear()
+    # display the artwork
+    print(logo)
+
+    # prompt for the two numbers and the operation symbol
+    num1 = int(input("What's the first number? "))
+
+    # enable the loop
+    should_continue = True
+
+    # begin main loop
+    while should_continue:
+        # show the available operations
+        for symbol in operations:
+            print(symbol, end=" ")
+        print()
+
+        # prompt for operation
+        operation_symbol = input("Pick an operation from the list above: ")
+        # prompt for num2
+        num2 = int(input("What's the second number? "))
+
+        # create the operation function
+        calculation_function = operations[operation_symbol]
+        # perform the operation
+        answer = calculation_function(num1, num2)
+
+        # display the result
+        print(f'{num1} {operation_symbol} {num2} = {answer}')
+
+        # prompt to continue with loop
+        option = input(
+            f"[C]ontinue with {answer}   [N]ew calculation   [Q]uit").lower()
+        # continue with answer
+        if option == 'c':
+            num1 = answer
+        # exit the loop
+        elif option == 'q':
+            break
+        # else stop loop and recursively start the calculator again
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
+
+# clear the screen, display art, exit the app.
 clear()
-
-# display the artwork
 print(logo)
-
-# show the available operations
-for symbol in operations:
-    print(symbol)
-
-# prompt for the two numbers and the operation symbol
-num1 = int(input("What's the first number? "))
-operation_symbol = input("Pick an operation from the list above: ")
-num2 = int(input("What's the second number? "))
-
-# create the operation function
-calculation_function = operations[operation_symbol]
-# perform the operation
-answer = calculation_function(num1, num2)
-
-# display the result
-print(f'{num1} {operation_symbol} {num2} = {answer}')
+print("Goodbye!")
