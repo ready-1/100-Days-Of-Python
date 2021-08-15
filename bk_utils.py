caec6ff234259118736438135c6a108877cb1ff1
Bob King <bob@getready1.com>
2021-08-14 21:58:34 -0400

    add docstring

--- bk_utils.py
+++ bk_utils.py
@@ -1,6 +1,12 @@
 from os import system, name
 
-def clear():  
+
+def clear():
+    """Clear the screen
+
+    Use the system clear command to clear the terminal screen.
+    This does not clear the history or scrollback.
+    """
     # for windows
     if name == 'nt':
         _ = system('cls')
